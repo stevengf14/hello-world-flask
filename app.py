d8232de0e17f48335e90a4e15d40e44185f80303
Steven <stevengf14@gmail.com>
2022-01-04 13:24:44 -0500

    Logging

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    app.logger.info(f'Get the path {request.path}')
     return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(debug=True)
