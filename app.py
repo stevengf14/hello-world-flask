1a4aeb4963c6941fc9aeaa9969f7c7849b82bd65
Steven <stevengf14@gmail.com>
2022-01-05 12:28:13 -0500

    Routing and POST Service

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ def hello_world():
     app.logger.info(f'Get the path {request.path}')
     return 'Hello World'
 
+@app.route('/hello/<name>')
+def hello(name):
+    return f'Hello {name.upper()}'
+
+@app.route('/age/<int:age>')
+def show_age(age):
+    return f'Your age is: {age}'
+
+@app.route('/name/<name>', methods=['GET','POST'])
+def show_name(name):
+    return f'Your name is: {name}'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug = True)
